09794a51137b2554711c14e38f57966ef2ec83e2
Ben Doe <benjamin.doe@outlook.com>
2018-03-05 11:18:34 +0000

    added is_admin

--- App_Data/dbo.Users.sql
+++ App_Data/dbo.Users.sql
@@ -1,6 +1,8 @@
-﻿CREATE TABLE [dbo].Users
-(
-	[id] INT NOT NULL PRIMARY KEY, 
-    [username] VARCHAR(50) NOT NULL, 
-    [password] VARCHAR(100) NOT NULL
-)
+﻿CREATE TABLE [dbo].[Users] (
+    [id]       INT           NOT NULL,
+    [username] VARCHAR (50)  NOT NULL,
+    [password] VARCHAR (100) NOT NULL,
+    [is_admin] BIT           NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
