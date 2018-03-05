8d7d29021c50f7cd6c0ab710e6e1c02eadbfba65
Ben Doe <benjamin.doe@outlook.com>
2018-03-05 10:41:55 +0000

    added games and users tables

---
+++ App_Data/dbo.Games.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [title] VARCHAR(50) NOT NULL, 
+    [description] TEXT NULL, 
+    [overall_rating] TINYINT NULL, 
+    [price] INT NOT NULL, 
+    [release_date] DATE NULL, 
+    [age_rating] VARCHAR(10) NULL
+)
---
+++ App_Data/dbo.Users.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].Users
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [username] VARCHAR(50) NOT NULL, 
+    [password] VARCHAR(100) NOT NULL
+)
