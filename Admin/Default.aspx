00f098c8fbdbaccd2c2ffea5a0406b29dd152fef
Ben Doe <benjamin.doe@outlook.com>
2018-03-05 12:31:32 +0000

    made start on admin

---
+++ Admin/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/MasterPage.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Admin_Default" %>
+
+<%-- Add content controls here --%>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</asp:Content>
+
---
+++ Admin/MasterPage.master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+public partial class Admin_MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["isAuth"] != null && (bool)Session["isAuth"])
+        {
+
+        } else
+        {
+            Response.Redirect("/Login.aspx");
+        }
+    }
+}
