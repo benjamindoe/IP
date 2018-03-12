b691113f684c63a9c7c403ceac4d32ee54b5bfd6
Ben Doe <benjamin.doe@outlook.com>
2018-03-12 12:14:43 +0000

    added auto increment to games

--- App_Data/dbo.Games.sql
+++ App_Data/dbo.Games.sql
@@ -1,10 +1,11 @@
-﻿CREATE TABLE [dbo].[Games]
-(
-	[id] INT NOT NULL PRIMARY KEY, 
-    [title] VARCHAR(50) NOT NULL, 
-    [description] TEXT NULL, 
-    [overall_rating] TINYINT NULL, 
-    [price] INT NOT NULL, 
-    [release_date] DATE NULL, 
-    [age_rating] VARCHAR(10) NULL
-)
+﻿CREATE TABLE [dbo].[Games] (
+    [id]             INT          NOT NULL IDENTITY,
+    [title]          VARCHAR (50) NOT NULL,
+    [description]    TEXT         NULL,
+    [overall_rating] TINYINT      NULL,
+    [price]          INT          NOT NULL,
+    [release_date]   DATE         NULL,
+    [age_rating]     VARCHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
