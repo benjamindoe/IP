d4fbc3a82c6cfc9a0cf2d7e2e7d72a06af200298
Ben Doe <benjamin.doe@outlook.com>
2018-03-13 00:27:34 +0000

    added image url column to games for cover art

--- App_Data/dbo.Games.sql
+++ App_Data/dbo.Games.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[Games] (
-    [id]             INT          IDENTITY (1, 1) NOT NULL,
-    [title]          VARCHAR (50) NOT NULL,
-    [description]    VARCHAR(MAX)         NULL,
-    [overall_rating] TINYINT      NULL,
-    [price]          INT          NOT NULL,
-    [release_date]   DATE         NULL,
-    [age_rating]     VARCHAR (10) NULL,
+    [id]             INT             IDENTITY (1, 1) NOT NULL,
+    [title]          VARCHAR (50)    NOT NULL,
+    [description]    VARCHAR (MAX)   NULL,
+    [overall_rating] TINYINT         NULL,
+    [price]          INT             NOT NULL,
+    [release_date]   DATE            NULL,
+    [age_rating]     VARCHAR (10)    NULL,
+    [image]          VARCHAR(100) NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
-
