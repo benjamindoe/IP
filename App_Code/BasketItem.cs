f05d9f434137b0a01187190051ebe49e7786014f
Ben Doe <benjamin.doe@outlook.com>
2018-03-19 21:46:24 +0000

    Added HTML output for Basket Items

--- App_Code/BasketItem.cs
+++ App_Code/BasketItem.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI.HtmlControls;
 
 public class BasketItem
 {
@@ -26,5 +27,44 @@ public class BasketItem
             + " at " + Product.Price.ToString("c") + " each)";
 
         return displayString;
+    }
+
+    public HtmlGenericControl ToHtml()
+    {
+        HtmlGenericControl item = new HtmlGenericControl("div");
+        item.Attributes.Add("class", "basket-item");
+
+        HtmlGenericControl htmlItem = new HtmlGenericControl("div");
+        HtmlImage img = new HtmlImage
+        {
+            Src = this.Product.ImagePath,
+        };
+        img.Attributes.Add("class", "item-img");
+        item.Controls.Add(img);
+
+        HtmlAnchor title = new HtmlAnchor
+        {
+            HRef = "/Games.aspx?id=" + this.Product.ID,
+            InnerText = this.Product.Title
+        };
+        title.Attributes.Add("class", "item-title");
+        item.Controls.Add(title);
+
+        HtmlGenericControl price = new HtmlGenericControl("div")
+        {
+            InnerHtml = "&pound;" + this.Product.Price
+        };
+        price.Attributes.Add("class", "item-price");
+        item.Controls.Add(price);
+
+        HtmlGenericControl quantity = new HtmlGenericControl("div")
+        {
+            InnerHtml = "&pound;" + this.Product.Price,
+            ID = "itemQuantity" + this.Product.ID
+        };
+        quantity.Attributes.Add("class", "item-quantity");
+        item.Controls.Add(quantity);
+
+        return item;
     }
 }
