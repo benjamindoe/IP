ca4fd9ed0faa9222ada5e450ac63b63f53a5af8e
Ben Doe <benjamin.doe@outlook.com>
2018-03-19 21:50:34 +0000

    Created basket view with initial code for empty basket and basket listing

---
+++ Basket.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MainSite.master" AutoEventWireup="true" CodeFile="Basket.aspx.cs" Inherits="Basket" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+    <form runat="server" id="form1">
+        <div class="basket" id="BasketControl" runat="server">
+
+        </div>
+    </form>
+</asp:Content>
+
---
+++ Basket.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+public partial class Basket : System.Web.UI.Page
+{
+    protected ShoppingBasket basket;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        basket = ShoppingBasket.GetBasket();
+
+        if (basket.Count > 0)
+        {
+            for (int i = 0; i < basket.Count; ++i)
+            {
+                BasketItem item = basket[i];
+                HtmlGenericControl html = item.ToHtml();
+                BasketControl.Controls.Add(html);
+            }
+        } else
+        {
+            HtmlGenericControl h2 = new HtmlGenericControl("h2")
+            {
+                InnerText = "Your Basket is Empty"
+            };
+            HtmlAnchor a = new HtmlAnchor
+            {
+                HRef = "/",
+                InnerText = "Continue Shopping"
+            };
+
+            BasketControl.Controls.Add(h2);
+            BasketControl.Controls.Add(a);
+        }
+    }
+}
