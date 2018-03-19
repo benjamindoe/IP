ac8b6c9ea2a506e7c73e31202fbe6616fcae09f3
Ben Doe <benjamin.doe@outlook.com>
2018-03-19 21:50:55 +0000

    Added initial code for game details page

---
+++ GameDetails.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MainSite.master" AutoEventWireup="true" CodeFile="GameDetails.aspx.cs" Inherits="GameDetails" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+    <form runat="server" id="form1">
+        <div class="game">
+            <img src="/" class="game-image" id="GameImage" runat="server"/>
+            <h2 runat="server" id="GameTitle"></h2>
+            <p runat="server" id="GameDesc"></p>
+            <asp:Button Text="Add to Basket" id="btnAddBasket" runat="server" />
+        </div>
+    </form>
+</asp:Content>
+
---
+++ GameDetails.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public partial class GameDetails : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.QueryString["id"] != null)
+        {
+            string id = Request.QueryString["id"];
+            string conStr = ConfigurationManager.ConnectionStrings["ConnectionString2"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT TOP 1 * FROM Games WHERE id = @id ", con))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        GameImage.Src = reader["image"].ToString();
+                        GameTitle.InnerText = reader["title"].ToString();
+                        GameDesc.InnerText = reader["description"].ToString();
+                        string price = reader["price"].ToString();
+                        string ageRating = reader["age_rating"].ToString();
+                        DateTime releaseDate = Convert.ToDateTime(reader["release_date"]);
+                    }
+                }
+                con.Close();
+            }
+        } else
+        {
+            Response.Redirect("/");
+        }
+    }
+}
