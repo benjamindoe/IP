4d63bba5facc03d5446234786d1ac9634bd4da0e
Ben Doe <benjamin.doe@outlook.com>
2018-03-19 16:13:03 +0000

    separated out header of website

---
+++ MainSite.master.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.UI.HtmlControls;
+
+public partial class MainSite : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["isAuth"] != null && (bool)Session["isAuth"])
+        {
+            ulNavBar.Controls.Remove(liCart);
+            ulNavBar.Controls.Remove(liLogin);
+
+            AddHeaderControl("/Profile.aspx", "<i class=\"material-icons\">person</i> Profile");
+
+            ulNavBar.Controls.Add(liCart);
+
+            string logoutPath = "/Logout.aspx?redirect=" + Server.UrlEncode(Request.Url.PathAndQuery);
+            AddHeaderControl(logoutPath, "<i class=\"material-icons\">power_settings_new</i> Logout");
+
+            if (Session["isAdmin"] != null && (bool)Session["isAdmin"])
+            {
+                AddHeaderControl("/Admin", "<i class=\"material-icons\">security</i>Admin");
+            }
+        }
+    }
+    protected void AddHeaderControl(string href, string html)
+    {
+
+        HtmlGenericControl li = new HtmlGenericControl("li");
+        HtmlAnchor a = new HtmlAnchor
+        {
+            HRef = href,
+            InnerHtml = html
+        };
+        li.Controls.Add(a);
+        ulNavBar.Controls.Add(li);
+    }
+}
--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -1,42 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.HtmlControls;
-using System.Web.UI.WebControls;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (Session["isAuth"] != null && (bool)Session["isAuth"])
-        {
-            ulNavBar.Controls.Remove(liCart);
-            ulNavBar.Controls.Remove(liLogin);
-
-            AddHeaderControl("/Profile.aspx", "<i class=\"material-icons\">person</i> Profile");
-
-            ulNavBar.Controls.Add(liCart);
-
-            string logoutPath = "/Logout.aspx?redirect=" + Server.UrlEncode(Request.Url.PathAndQuery);
-            AddHeaderControl(logoutPath, "<i class=\"material-icons\">power_settings_new</i> Logout");
-
-            if (Session["isAdmin"] != null && (bool)Session["isAdmin"])
-            {
-                AddHeaderControl("/Admin", "<i class=\"material-icons\">security</i>Admin");
-            }
-        }
-    }
-
-    protected void AddHeaderControl(string href, string html)
-    {
-
-        HtmlGenericControl li = new HtmlGenericControl("li");
-        HtmlAnchor a = new HtmlAnchor();
-        a.HRef = href;
-        a.InnerHtml = html;
-        li.Controls.Add(a);
-        ulNavBar.Controls.Add(li);
     }
 }
