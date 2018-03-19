28d203f9f76013cd89c66c7240dcd0736786119c
Ben Doe <benjamin.doe@outlook.com>
2018-03-19 16:18:23 +0000

    create Auth Master page to fake middleware

---
+++ AuthMaster.master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AuthMaster : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["isAuth"] != null && (bool)Session["isAuth"])
+        {
+
+        }
+        else
+        {
+            Response.Redirect("/Login.aspx?redirect=" + Server.UrlEncode(Request.Url.PathAndQuery));
+        }
+    }
+}
