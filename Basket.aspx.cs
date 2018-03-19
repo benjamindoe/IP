30f1cda43a2d2ad6b0fe84e450e2ddff85e3c3f0
Ben Doe <benjamin.doe@outlook.com>
2018-03-19 23:21:58 +0000

    Added checkout functionality from Yr 2 work. Needs testing

--- Basket.aspx.cs
+++ Basket.aspx.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.HtmlControls;
+using System.Configuration;
+using System.Data.SqlClient;
 
 public partial class Basket : System.Web.UI.Page
 {
@@ -27,14 +29,61 @@ public partial class Basket : System.Web.UI.Page
             {
                 InnerText = "Your Basket is Empty"
             };
-            HtmlAnchor a = new HtmlAnchor
-            {
-                HRef = "/",
-                InnerText = "Continue Shopping"
-            };
-
             BasketControl.Controls.Add(h2);
-            BasketControl.Controls.Add(a);
+            Page.Controls.Remove(btnCheckout);
+        }
+    }
+
+    protected void btnCheckout_Click(object sender, EventArgs e)
+    {
+        if (Session["isLoggedIn"] as bool? ?? false)
+        {
+            ShoppingBasket basket = ShoppingBasket.GetBasket();
+            string username = (string)Session["username"];
+            int userId = int.Parse(Session["userId"].ToString());
+            int? newID = null;
+            string conStr = ConfigurationManager.ConnectionStrings["ConnectionString2"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO [Orders]([user_id], [subtotal], [date]) OUTPUT INSERTED.ID VALUES (@username, @subtotal, GETDATE())", con))
+                {
+                    cmd.Parameters.AddWithValue("@userId", userId);
+                    cmd.Parameters.AddWithValue("@subtotal", basket.SubTotal);
+                    try
+                    {
+                        newID = (int?)cmd.ExecuteScalar();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                if (newID != null)
+                {
+                    for (int i = 0; newID != null && i < basket.Count; i++)
+                    {
+                        using (SqlCommand cmd = new SqlCommand("INSERT INTO OrderItems ([order_id],[game_id],[quantity]) VALUES (@orderId, @gameId, @quantity)", con))
+                        {
+                            cmd.Parameters.AddWithValue("@orderId", newID);
+                            cmd.Parameters.AddWithValue("@gameId", basket[i].Product.ID);
+                            cmd.Parameters.AddWithValue("@quantity", basket[i].Quantity);
+                            cmd.ExecuteScalar();
+                        }
+                    }
+                    if (basket.Count > 0)
+                    {
+                        basket.Clear();
+                    }
+                }
+                con.Close();
+                Response.Redirect("/Orders.aspx");
+            }
+
+        } else
+        {
+            string loginUrl = "/login.aspx?redirect=" + Server.UrlEncode(Request.Url.PathAndQuery);
+            Response.Redirect(loginUrl);
         }
     }
 }
