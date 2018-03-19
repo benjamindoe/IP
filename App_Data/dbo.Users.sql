4b9c6a286b5053126baa2ca72e7a13070328690a
Ben Doe <benjamin.doe@outlook.com>
2018-03-19 16:16:20 +0000

    added defaults to users table

--- App_Data/dbo.Users.sql
+++ App_Data/dbo.Users.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Users] (
-    [id]       INT           NOT NULL IDENTITY,
+    [id]       INT           IDENTITY (1, 1) NOT NULL,
     [username] VARCHAR (50)  NOT NULL,
     [password] VARCHAR (100) NOT NULL,
-    [is_admin] BIT           NULL,
+    [is_admin] BIT           NULL DEFAULT 0,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
