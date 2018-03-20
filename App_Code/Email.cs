2b0443a5721d5f91054f2b0179c26fb33621d750
Ben Doe <benjamin.doe@outlook.com>
2018-03-20 00:09:17 +0000

    Added SMTP email class

---
+++ App_Code/Email.cs
@@ -0,0 +1,75 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Net;
+using System.Net.Mail;
+
+/// <summary>
+/// Summary description for Email
+/// </summary>
+public class Email
+{
+    protected SmtpClient client;
+    public Email()
+    {
+
+    }
+    public Email(string to)
+    {
+        EmailTo = to;
+        client = new SmtpClient("smtp.mailgun.org")
+        {
+            Credentials = new NetworkCredential(
+            "postmaster@sandbox619e95ee455844d5841175a8d4d14b19.mailgun.org",
+            "ff3fc388cd16108ef14b7f9716bcfc0f-db1f97ba-57e9c9bb"
+            )
+        };
+    }
+
+    public string Subject { set; get; }
+
+    public string Body { set; get; }
+
+    public string EmailTo { set; get; }
+
+    //public static void SendSimpleMessage()
+    //{
+    //    RestClient client = new RestClient();
+    //    client.BaseUrl = "https://api.mailgun.net/v3";
+    //    client.Authenticator =
+    //    new HttpBasicAuthenticator("api",
+    //                              "key-64ba373c08997e028b2cd5b872ebefcb");
+    //    RestRequest request = new RestRequest();
+    //    request.AddParameter("domain", "sandbox619e95ee455844d5841175a8d4d14b19.mailgun.org", ParameterType.UrlSegment);
+    //    request.Resource = "{domain}/messages";
+    //    request.AddParameter("from", "Mailgun Sandbox <postmaster@sandbox619e95ee455844d5841175a8d4d14b19.mailgun.org>");
+    //    request.AddParameter("to", "Ben Doe <benjamin.doe@outlook.com>");
+    //    request.AddParameter("subject", "Hello Ben Doe");
+    //    request.AddParameter("text", "Congratulations Ben Doe, you just sent an email with Mailgun!  You are truly awesome!");
+    //    request.Method = Method.POST;
+    //    return client.Execute(request);
+    //}
+
+    public void Send(string emailFrom)
+    {
+        MailAddress mailTo = new MailAddress(EmailTo);
+        MailAddress mailFrom = new MailAddress(emailFrom);
+        MailMessage message = new MailMessage(mailFrom, mailTo)
+        {
+            Subject = Subject,
+            Body = Body
+        };
+        // Use the application or machine configuration to get the 
+        // host, port, and credentials.
+        System.Diagnostics.Debug.WriteLine("Sending an e-mail message to {0} at {1} by using the SMTP host={2}.",
+            mailTo.User, mailTo.Host, client.Host);
+        try
+        {
+            client.Send(message);
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine("Exception caught in CreateTestMessage3(): {0}", ex.ToString());
+        }
+    }
+}
