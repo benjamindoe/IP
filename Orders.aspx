3c332f9263999ab088001d37df650f3028a29c45
Ben Doe <benjamin.doe@outlook.com>
2018-04-02 20:33:26 +0100

    orders for a user page

---
+++ Orders.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AuthMaster.master" AutoEventWireup="true" CodeFile="Orders.aspx.cs" Inherits="Orders" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+    <form id="form1" runat="server">
+        <h1>Your Orders</h1>
+        <ul id="ulOrders" class="orders" runat="server">
+
+        </ul>
+    </form>
+</asp:Content>
+
---
+++ Orders.aspx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+public partial class Orders : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString2"].ConnectionString))
+        {
+            con.Open();
+            string query = @"SELECT
+                                [Orders].[id],
+                                [Orders].[subtotal],
+                                [Orders].[date],
+                                [Games].[title],
+                                [Games].[price],
+                                [Games].[image],
+                                [OrderItems].[quantity] 
+                            FROM
+                                Orders 
+                            INNER JOIN
+                                [OrderItems] 
+                            ON
+                                [OrderItems].[order_id] = [Orders].[id] 
+                            INNER JOIN
+                                [Games] 
+                            ON
+                                [Games].[id] = [OrderItems].[game_id]
+                            WHERE
+                                [Orders].[user_id] = @id";
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                cmd.Parameters.AddWithValue("@id", Session["userId"].ToString());
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while(reader.Read())
+                    {
+                        BuildOrder(reader);
+                    }
+                }
+            }
+        }
+    }
+
+    protected void BuildOrder(SqlDataReader reader)
+    {
+        string orderId = "Order" + reader["id"].ToString();
+        HtmlGenericControl order = (HtmlGenericControl)ulOrders.FindControl(orderId);
+        HtmlGenericControl orderItems;
+        if (order == null) // If control doesn't exist, add it.
+        {
+            order = new HtmlGenericControl("li")
+            {
+                ID = orderId,
+            };
+            order.Attributes.Add("class", "order");
+            orderItems = new HtmlGenericControl("ul")
+            {
+                ID = orderId + "Items"
+            };
+            orderItems.Attributes.Add("class", "order-items");
+            HtmlGenericControl orderHeader = new HtmlGenericControl("div");
+            orderHeader.Attributes.Add("class", "order-header");
+            order.Controls.Add(orderHeader);
+            string[] orderDetails = new string[]{ "id", "subtotal", "date"};
+            foreach (var detail in orderDetails)
+            {
+                var divDetail = new HtmlGenericControl("div");
+                switch (detail)
+                {
+                    case "id":
+                        divDetail.InnerText = "ORDER  #" + reader[detail].ToString();
+                        break;
+                    case "subtotal":
+                        var subtotal = (decimal)reader[detail];
+                        divDetail.InnerText = "Total: £" + Math.Round(subtotal, 2);
+                        break;
+                    case "date":
+                        var date = Convert.ToDateTime(reader[detail]);
+                        divDetail.InnerText = "Order Date: " + date.ToString("dd MMMM yyyy");
+                        break;
+                }
+                orderHeader.Controls.Add(divDetail);
+            }
+            HtmlGenericControl orderBody = new HtmlGenericControl("div");
+            orderBody.Attributes.Add("class", "order-body");
+            order.Controls.Add(orderBody);
+            var h2 = new HtmlGenericControl("h2")
+            {
+                InnerText = "Ordered Items"
+            };
+            orderBody.Controls.Add(h2);
+            orderBody.Controls.Add(orderItems);
+            ulOrders.Controls.Add(order);
+        } else
+        {
+            orderItems = (HtmlGenericControl)order.FindControl(orderId + "Items");
+        }
+        BuildOrderItems(ref orderItems, reader);
+
+    }
+
+    protected void BuildOrderItems(ref HtmlGenericControl ulItems, SqlDataReader reader)
+    {
+        string[] details = new string[] { "image", "title", "price" };
+        HtmlGenericControl liItem = new HtmlGenericControl("li");
+        liItem.Attributes.Add("class", "item");
+        HtmlGenericControl div;
+        foreach (var detail in details)
+        {
+            div = new HtmlGenericControl("div");
+            div.Attributes.Add("class", "item-" + detail);
+            switch (detail)
+            {
+                case "title":
+                    div.InnerHtml = reader["quantity"].ToString() + " x " + reader["title"].ToString();
+                    
+                    break;
+                case "image":
+                    HtmlImage image = new HtmlImage
+                    {
+                        Src = reader[detail].ToString()
+                    };
+                    div.Controls.Add(image);
+                    break;
+                case "price":
+                    div.InnerHtml = Server.HtmlEncode("£" + reader[detail].ToString());
+                    break;
+                default:
+                    div.InnerHtml = Server.HtmlEncode(reader[detail].ToString());
+                    break;
+            }
+            liItem.Controls.Add(div);
+        }
+        ulItems.Controls.Add(liItem);
+    }
+}
