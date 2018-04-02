3a5abc698d340b68a9c8710531cb07b9f381cba7
Ben Doe <benjamin.doe@outlook.com>
2018-04-02 20:34:20 +0100

    Added user rows for profile page editing stuff

--- App_Data/dbo.Users.sql
+++ App_Data/dbo.Users.sql
@@ -2,7 +2,11 @@
     [id]       INT           IDENTITY (1, 1) NOT NULL,
     [username] VARCHAR (50)  NOT NULL,
     [password] VARCHAR (100) NOT NULL,
-    [is_admin] BIT           NULL DEFAULT 0,
+    [is_admin] BIT           DEFAULT 0 NULL,
+    [firstname] VARCHAR(100) NULL, 
+    [surname] VARCHAR(100) NULL, 
+    [phone] VARCHAR(15) NULL, 
+    [email] VARCHAR(150) NULL, 
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
